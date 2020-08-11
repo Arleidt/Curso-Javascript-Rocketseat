454b4d914b1c8a3a9fbe8206bedff4b58efa2587
arlei duarte <arleidt@yahoo.com.br>
2020-08-11 14:09:36 -0300

    ajuste produto

--- node-api/src/models/Product.js
+++ node-api/src/models/Product.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//na var passo um objeto e nesse obj os campos que quero salvar no banco pra cada produto
 const ProductSchema = new mongoose.Schema({
   title:{
     type: String, 
@@ -16,8 +17,14 @@ const ProductSchema = new mongoose.Schema({
   },
   created:{
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
 });
 
+//registra model na aplicação, toda aplicação vai saber que existe um model Product.
+//E que possui as propriedades acima
 mongoose.model("Product", ProductSchema);
+//url link para produto
+//created se preenche automaticamente essa coluna vai salvar uma data
+//e vai se preencher automaticamente com o valor da data atual que o registro 
+//for criado no banco de dados
