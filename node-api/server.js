cc5ce8f5fd52b7d4d5c385546c3b71741e4897d6
arlei duarte <arleidt@yahoo.com.br>
2020-08-11 01:27:40 -0300

    criação modulo produto instalação mongo

---
+++ node-api/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//iniciando o app
+const app = express();
+
+//Iniciando o DB
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapirock', 
+  {useNewUrlParser: true}
+);
+//require('./src/models/Product');
+//npm install require-dir faz require em um diretorio em todos os arq automaticamente
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+//primeira rota
+app.get('/', (req, res) => {
+  Product.create({title: 'React Native',
+   description: 'Build native apps with React',
+   url:'https://github.com/Arleidt'
+  });
+
+  res.send("Hello World");
+});
+  
+
+app.listen(3002);
